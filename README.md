a45a90ba1ab0e5c45c328f72548159444bb69235
eren <akbaseren4751@gmail.com>
2022-08-26 18:51:25 +0300

    Add main module files and overlays.

---
+++ README.md
@@ -0,0 +1,26 @@
+# Extra-Overlays
+A magisk module that adds more WiFi and Signal icons to Android 12 / 12.1 AOSP roms.
+
+- Signal Icons Before
+ 
+![Before](https://github.com/MrErenK/Extra-Overlays/blob/screenshots/signal_icons/before.jpg?raw=true)
+
+- Signal Icons After
+
+![After](https://github.com/MrErenK/Extra-Overlays/blob/screenshots/signal_icons/after.jpg?raw=true)
+
+- WiFi Icons Before
+ 
+![Before](https://github.com/MrErenK/Extra-Overlays/blob/screenshots/wifi_icons/before.jpg?raw=true)
+
+- WiFi Icons After
+ 
+![After](https://github.com/MrErenK/Extra-Overlays/blob/screenshots/wifi_icons/after.jpg?raw=true)
+
+- Icon Packs Before
+ 
+![Before](https://github.com/MrErenK/Extra-Overlays/blob/screenshots/icon_packs/before.jpg?raw=true)
+
+- Icon Packs After
+ 
+![After](https://github.com/MrErenK/Extra-Overlays/blob/screenshots/icon_packs/after.jpg?raw=true)
---
+++ setup.sh
@@ -0,0 +1,81 @@
+#!/sbin/sh
+
+###########################
+# MMT Reborn Logic
+###########################
+
+############
+# Config Vars
+############
+
+# Set this to true if you want to skip mount for your module
+SKIPMOUNT="false"
+# Set this to true if you want to clean old files in module before injecting new module
+CLEANSERVICE="false"
+# Set this to true if you want to load vskel after module info print. If you want to manually load it, consider using load_vksel function
+AUTOVKSEL="false"
+# Set this to true if you want store debug logs of installation
+DEBUG="true"
+
+############
+# Replace List
+############
+
+# List all directories you want to directly replace in the system
+# Construct your list in the following example format
+REPLACE_EXAMPLE="
+/system/app/Youtube
+/system/priv-app/SystemUI
+/system/priv-app/Settings
+/system/framework
+"
+# Construct your own list here
+REPLACE="
+"
+
+############
+# Permissions
+############
+
+# Set permissions
+set_permissions() {
+  set_perm_recursive "$MODPATH" 0 0 0777 0755
+  set_perm_recursive "$MODPATH/system/product/overlay" 0 0 0777 0755
+}
+
+############
+# Info Print
+############
+
+# Set what you want to be displayed on header of installation process
+info_print() {
+  ui_print ""
+  ui_print "*********************************"
+  ui_print "         Extra Overlays"
+  ui_print "     By Mr_Erenk & SkEpTRoN"
+  ui_print "*********************************"
+
+  sleep 1
+}
+
+############
+# Main
+############
+
+# Change the logic to whatever you want
+init_main() {
+  ui_print ""
+  ui_print "[*] Installing Extra Overlays..."
+
+  sleep 1
+
+  ui_print ""
+  ui_print "[*] Done!"  
+  
+  sleep 1
+
+  ui_print ""
+  ui_print "[*] Reboot is required"
+  
+  sleep 1.5
+}
